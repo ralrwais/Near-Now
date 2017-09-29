606bb418ec1d10ffe5eefdfb0d0e6a38b9ea30f6
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-09-29 15:22:25 -0700

    quickest app ever built

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Near-Now
+# Near Now
+
+## Find only whats near you and open now!
+
+Near Now is a search web application that allows users to search for places that are both near them and open currently. It narrows down the search to top 10 results making it quicker and easier to find somewhere to go, and eliminating any distracting and un-useful information.  
+
+## The Tech:
+
+* Javascript 
+* Google Places API
+* Bootstrap
+* CSS 💅🏽
+ 
+
+#### Happy Searching! 
---
+++ index.js
@@ -0,0 +1,98 @@
+var infowindow;
+
+var sanfran = new google.maps.LatLng(37.773972, -122.431297);
+
+
+var map = new google.maps.Map(document.getElementById('map'), {
+		center: sanfran,
+		zoom: 13
+	});
+
+
+$(".searchButton").on("click", function() {
+	var input = $("#searchBox").val();
+
+	var request = {
+		location: sanfran,
+		radius: '500',
+		query: input,
+		openNow: true
+
+	};
+	infowindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+	service.textSearch(request, callback);
+
+})
+
+function callback(results, status) {
+	
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for(var i = 0; i < 10; i++){
+			if(results[i].photos) {
+			var place = results[i];
+			var placeID = place.place_id;	
+			createMarker(place)
+
+			var request = {
+  				placeId: placeID
+			};
+
+		service = new google.maps.places.PlacesService(map);
+		service.getDetails(request, callback);
+
+		function callback(place, status) {
+  		if (status == google.maps.places.PlacesServiceStatus.OK) {
+  			var details = place;
+  			
+    		var name = details.name
+    		
+    		var address = details.formatted_address;
+    		
+    		var website = details.website;
+    		
+    		var pic = details.photos[0].getUrl({'maxWidth': 250, 'maxHeight': 250});
+    		
+    		var rating = details.rating + ' Stars';
+
+    		var open = 'Open Now';
+
+    		var template = `	
+				<div class="card" style="width: 20rem;">
+                      <img class="card-img-top img" src=${pic} alt="Card image cap">
+                      <div class="card-block">
+                        <h4 class="card-title" id="name">${name}</h4>
+                        <p class="card-text" id="address">${address}</p>
+                      </div>
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item rating" id="rating">${rating}</li>
+                        <li class="list-group-item open" id="open">${open}</li>
+                      </ul>
+                      <div class="card-block">
+                        <a href=${website} class="card-link" id="website"> Visit Website </a>
+                      </div>
+                    </div>`
+
+             var myCard = $('#media').append(template);
+
+ 		 }
+		}
+	  }
+  	}
+	}
+}
+
+	 function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+
+
